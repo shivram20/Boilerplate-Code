3b1042c5d734b5a5f12202f30b446aed34667626
shivram20 <shivramvasava20@gmail.com>
2026-09-11 12:48:37 +0530

    new repo branch

---
+++ MERN/backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+const cors = require('cors')
+
+dotenv.config()
+const port = process.env.PORT || 5000
+const server = express()
+
+
+// Middleware Setup
+server.use(express.json())
+
+
+// Routes Setup
+server.get("/",(req,res)=>{
+    res.send("Hello from API");
+    res.end()
+})
+
+server.listen(port,() =>{
+    console.log(`server start on port ${port}`)
+})
